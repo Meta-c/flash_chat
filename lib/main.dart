2be45b0ccaf575b4db435e277a020234f7f0e18a
Meta-c <metamedhat116@gmail.com>
2022-09-30 05:32:16 +0200

    Routes to static variables

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,12 @@ class FlashChat extends StatelessWidget {
           bodyText1: TextStyle(color: Colors.black54),
         ),
       ),
-      initialRoute: 'welcome',
+      initialRoute: WelcomeScreen.id,
       routes: {
-        'login': (context) => LoginScreen(),
-        'registration': (context) => RegistrationScreen(),
-        'welcome': (context) => WelcomeScreen(),
-        'chat': (context) => ChatScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
       },
     );
   }
