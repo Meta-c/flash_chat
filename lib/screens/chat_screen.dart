ba0e7431aa9accb286be30fc9603a55877f60790
Meta-c <metamedhat116@gmail.com>
2022-10-03 11:53:53 +0200

    retrieving messages from firebase

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat';
@@ -9,8 +10,10 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final _fireStore = FirebaseFirestore.instance;
   final _auth = FirebaseAuth.instance;
   late User loggedInUser;
+  String messageText = '';
 
   void getCurrentUser() async {
     try {
@@ -24,6 +27,19 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
+  // void getMessages() async {
+  //   final messages = await _fireStore.collection('messages').get();
+
+  //   for (var message in messages.docs) print(message.data());
+  // }
+
+  void messagesStream() async {
+    await for (var snapshot in _fireStore.collection('messages').snapshots())
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -34,23 +50,49 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: null,
+        automaticallyImplyLeading: false,
+        //leading: null,
         actions: <Widget>[
           IconButton(
-              icon: Icon(Icons.close),
+              icon: Icon(Icons.logout),
               onPressed: () {
-                _auth.signOut();
-                Navigator.pop(context);
+                messagesStream();
+                // _auth.signOut();
+                // Navigator.pop(context);
               }),
         ],
-        title: Text('⚡️Chat'),
-        backgroundColor: Colors.lightBlueAccent,
+        title: Text('Flash Chat'),
+        backgroundColor: Color.fromARGB(95, 131, 121, 121),
       ),
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+              stream: _fireStore.collection('messages').snapshots(),
+              builder: (context, snapshot) {
+                List<Text> messageWidgets = [];
+                if (snapshot.hasData) {
+                  final messages = snapshot.data!.docs;
+
+                  for (var message in messages) {
+                    final messageText = message.get('Text');
+                    final messageSender = message.get('Sender');
+                    final messageWidget =
+                        Text('$messageSender said $messageText');
+                    messageWidgets.add(messageWidget);
+                  }
+
+                  return Column(
+                    children: messageWidgets,
+                  );
+                } else
+                  return Column(
+                    children: [Text('ERROR')],
+                  );
+              },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -59,14 +101,15 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   FlatButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      _fireStore.collection('messages').add(
+                          {'Text': messageText, 'Sender': loggedInUser.email});
                     },
                     child: Text(
                       'Send',
