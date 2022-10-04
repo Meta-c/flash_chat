8fa3af6a0cfb975acb7d71a0d8d500ed4ee1f4aa
Meta-c <metamedhat116@gmail.com>
2022-10-04 10:23:23 +0200

    added device preview package

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/screens/welcome_screen.dart';
 import 'package:flash_chat/screens/login_screen.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(FlashChat());
+  runApp(DevicePreview(
+      enabled: !kReleaseMode, builder: ((context) => FlashChat())));
 }
 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
           bodyText1: TextStyle(color: Colors.black54),
